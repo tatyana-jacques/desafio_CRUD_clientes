5eef8a5aae55c80237b7fbf586d81d752b285697
Tatyana Jacques <taty.aj@gmail.com>
2025-08-28 19:48:15 -0300

    Upload project

---
+++ src/test/java/com/tatyanajacques/desafio_CRUD_clientes/DesafioCrudClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tatyanajacques.desafio_CRUD_clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioCrudClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
