f8d65948d0b9d19f71c48d600c484e1f1b050a05
Tatyana Jacques <taty.aj@gmail.com>
2025-08-29 11:50:37 -0300

    GET clients

--- src/main/java/com/tatyanajacques/desafio_CRUD_clientes/controllers/ClientController.java
+++ src/main/java/com/tatyanajacques/desafio_CRUD_clientes/controllers/ClientController.java
@@ -3,6 +3,8 @@ package com.tatyanajacques.desafio_CRUD_clientes.controllers;
 import com.tatyanajacques.desafio_CRUD_clientes.dto.ClientDTO;
 import com.tatyanajacques.desafio_CRUD_clientes.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +22,12 @@ public class ClientController {
     public ResponseEntity<ClientDTO> findById(@PathVariable Long id){
 
         ClientDTO dto = service.findById(id);
+        return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ClientDTO>> findAll(Pageable pageable){
+        Page<ClientDTO> dto = service.findAll(pageable);
         return ResponseEntity.ok(dto);
     }
 
--- src/main/java/com/tatyanajacques/desafio_CRUD_clientes/services/ClientService.java
+++ src/main/java/com/tatyanajacques/desafio_CRUD_clientes/services/ClientService.java
@@ -5,6 +5,8 @@ import com.tatyanajacques.desafio_CRUD_clientes.entities.Client;
 import com.tatyanajacques.desafio_CRUD_clientes.repositories.ClientRepository;
 import com.tatyanajacques.desafio_CRUD_clientes.services.exceptions.IdNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,5 +22,11 @@ public class ClientService {
                 ()-> new IdNotFoundException("Cliente não encontrado."));
         return new ClientDTO(client);
 
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> findAll(Pageable pageable){
+        Page<Client> result = repository.findAll(pageable);
+        return result.map(x -> new ClientDTO(x));
     }
 }
